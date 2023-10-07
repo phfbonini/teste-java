f2934fdd03cf509a7be38a8c4d8151292c72228e
phfbonini <phf_apps@outlook.com>
2023-10-07 14:05:46 -0300

    implementação da criação das tabelas, já que ao executar a application ela deve criar as tabelas - verificação se as tabelas ja foram criadas

--- vr-vendas/src/service/DatabaseService.java
+++ vr-vendas/src/service/DatabaseService.java
@@ -18,7 +18,60 @@ public class DatabaseService {
         }
     }
 
+    public static void createTables() {
+        Connection connection = getConnection();
+        try {
+            Statement statement = connection.createStatement();
 
+            // Verifica se as tabelas já existem
+            ResultSet resultSet = statement.executeQuery("SELECT tablename FROM pg_catalog.pg_tables WHERE schemaname = 'public'");
+            boolean produtosTableExists = false;
+            boolean clientesTableExists = false;
+            boolean vendasTableExists = false;
+            boolean itensVendaTableExists = false;
+
+            while (resultSet.next()) {
+                String tableName = ((ResultSet) resultSet).getString("tablename");
+                if (tableName.equals("produtos")) {
+                    produtosTableExists = true;
+                } else if (tableName.equals("clientes")) {
+                    clientesTableExists = true;
+                } else if (tableName.equals("vendas")) {
+                    vendasTableExists = true;
+                } else if (tableName.equals("itens_venda")) {
+                    itensVendaTableExists = true;
+                }
+            }
+
+            // Crie as tabelas que não existem
+            if (!produtosTableExists) {
+                statement.executeUpdate("CREATE TABLE produtos (id serial PRIMARY KEY, descricao VARCHAR(255), preco NUMERIC, quantidade INTEGER)");
+            }
+
+            if (!clientesTableExists) {
+                statement.executeUpdate("CREATE TABLE clientes (id serial PRIMARY KEY, nome VARCHAR(255))");
+            }
+
+            if (!vendasTableExists) {
+                statement.executeUpdate("CREATE TABLE vendas (id serial PRIMARY KEY, data DATE, cliente_id INTEGER, valor_total NUMERIC, status VARCHAR(255))");
+            }
+
+            if (!itensVendaTableExists) {
+                statement.executeUpdate("CREATE TABLE itens_venda (id serial PRIMARY KEY, produto_id INTEGER, venda_id INTEGER, quantidade INTEGER, preco NUMERIC, valor_total NUMERIC)");
+            }
+
+            // Crie as relações entre as tabelas, se necessário
+            if (!produtosTableExists || !vendasTableExists || !itensVendaTableExists) {
+                // Crie chaves estrangeiras para vincular produtos, vendas e itens de venda
+                statement.executeUpdate("ALTER TABLE itens_venda ADD FOREIGN KEY (produto_id) REFERENCES produtos (id)");
+                statement.executeUpdate("ALTER TABLE itens_venda ADD FOREIGN KEY (venda_id) REFERENCES vendas (id)");
+            }
+
+            statement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
     public static Connection getConnection() {
         return connection;
@@ -33,6 +86,8 @@ public class DatabaseService {
         } else {
             System.out.println("Conexão com o banco de dados não está ativa.");
         }
-        
+
+        // Crie as tabelas quando a aplicação for iniciada
+        createTables();
     }
 }
