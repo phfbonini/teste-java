3f85ac0e14276616cdae93ea0449c480151c680a
phfbonini <phf_apps@outlook.com>
2023-10-07 22:21:57 -0300

    Implementação do Cadastro de Produto e da tela

--- vr-vendas/src/service/ProdutoService.java
+++ vr-vendas/src/service/ProdutoService.java
@@ -1,2 +1,55 @@
-package service;public class ProdutoService {
+package service;
+
+import model.Produto;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProdutoService {
+    public static List<Produto> getAllProdutos() {
+        List<Produto> produtos = new ArrayList<>();
+        Connection connection = DatabaseService.getConnection();
+
+        try {
+            String sql = "SELECT * FROM produtos";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            ResultSet resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String descricao = resultSet.getString("descricao");
+                double preco = resultSet.getDouble("preco");
+                int quantidade = resultSet.getInt("quantidade");
+                Produto produto = new Produto(id, descricao, preco, quantidade);
+                produtos.add(produto);
+            }
+
+            resultSet.close();
+            statement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return produtos;
+    }
+
+    public static void cadastrarProduto(String descricao, double preco, int quantidade) {
+        Connection connection = DatabaseService.getConnection();
+
+        try {
+            String sql = "INSERT INTO produtos (descricao, preco, quantidade) VALUES (?, ?, ?)";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setString(1, descricao);
+            statement.setDouble(2, preco);
+            statement.setInt(3, quantidade);
+            statement.executeUpdate();
+            statement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
---
+++ vr-vendas/src/ui/CadastroProdutoUI.java
@@ -0,0 +1,77 @@
+package ui;
+
+import service.ProdutoService;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class CadastroProdutoUI {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Cadastro de Produto");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 200);
+
+        JPanel panel = new JPanel();
+        frame.add(panel);
+        placeComponents(panel);
+
+        frame.setVisible(true);
+    }
+
+    private static void placeComponents(JPanel panel) {
+        panel.setLayout(null);
+
+        JLabel descricaoLabel = new JLabel("Descrição:");
+        descricaoLabel.setBounds(10, 10, 80, 25);
+        panel.add(descricaoLabel);
+
+        JTextField descricaoText = new JTextField(165);
+        descricaoText.setBounds(100, 10, 160, 25);
+        panel.add(descricaoText);
+
+        JLabel precoLabel = new JLabel("Preço:");
+        precoLabel.setBounds(10, 40, 80, 25);
+        panel.add(precoLabel);
+
+        JTextField precoText = new JTextField(165);
+        precoText.setBounds(100, 40, 160, 25);
+        panel.add(precoText);
+
+        JLabel quantidadeLabel = new JLabel("Quantidade:");
+        quantidadeLabel.setBounds(10, 70, 80, 25);
+        panel.add(quantidadeLabel);
+
+        JTextField quantidadeText = new JTextField(165);
+        quantidadeText.setBounds(100, 70, 160, 25);
+        panel.add(quantidadeText);
+
+        JButton cadastrarButton = new JButton("Cadastrar");
+        cadastrarButton.setBounds(100, 110, 100, 25);
+        panel.add(cadastrarButton);
+
+        cadastrarButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String descricao = descricaoText.getText();
+                String precoStr = precoText.getText();
+                String quantidadeStr = quantidadeText.getText();
+
+                if (!descricao.isEmpty() && !precoStr.isEmpty() && !quantidadeStr.isEmpty()) {
+                    try {
+                        double preco = Double.parseDouble(precoStr);
+                        int quantidade = Integer.parseInt(quantidadeStr);
+                        ProdutoService.cadastrarProduto(descricao, preco, quantidade);
+                        JOptionPane.showMessageDialog(null, "Produto cadastrado com sucesso!");
+                        descricaoText.setText("");
+                        precoText.setText("");
+                        quantidadeText.setText("");
+                    } catch (NumberFormatException ex) {
+                        JOptionPane.showMessageDialog(null, "Preço e quantidade devem ser números válidos.");
+                    }
+                } else {
+                    JOptionPane.showMessageDialog(null, "Por favor, preencha todos os campos.");
+                }
+            }
+        });
+    }
+}
